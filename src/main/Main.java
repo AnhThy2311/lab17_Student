4bac6b2ad2a1887c47e3bb7bdd246aa69d19322b
HP <HP@DESKTOP-TK2OJN6>
2023-10-20 09:20:14 +0700

    commit 17

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import controller.ManageSortInforStudent;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args) {
+        String title= "====== Collection Sort Program ======";
+        String[] s = new String[] {"Input Information Student", "Display", "Exit"};
+        new ManageSortInforStudent(title, s).run();
+    }
+}
